ea2cc235b4aa38bc792c983e6bae10ad511c4f03
Zach Nelson <ZachN@knowledgeservices.com>
2019-01-22 14:29:16 -0500

    added OwinHost

--- OWIN_Server/Startup.cs
+++ OWIN_Server/Startup.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Owin;
 
 namespace OWIN_Server
 {
     public class Startup
     {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello World!");
+            });
+        }
     }
 }
